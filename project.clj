ab724cb870ef89973964085faf9ca71f9076e629
Juha Jokimaki <juha.jokimaki@solita.fi>
2016-11-09 14:12:24 +0200

    Add distribution info

--- project.clj
+++ project.clj
@@ -1,8 +1,9 @@
 (defproject lupapiste/geo-conversion "0.1.0"
-  :description "epsg3067 to  wgs84 conversion library"
+  :description "epsg3067 to wgs84 conversion library"
   :url "http://www.solita.fi"
   :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+            :url "http://www.eclipse.org/legal/epl-v10.html"
+            :distribution :repo}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.geotools/gt-main "14.2"]
                  [org.geotools/gt-referencing "14.2"]
