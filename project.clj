103ac32d178bf0442fad7468fe695ddad5005772
Markus Penttila <markus.penttila@solita.fi>
2017-07-10 18:04:55 +0300

    Support conversion from and to arbitrary EPSG codes

--- project.clj
+++ project.clj
@@ -1,12 +1,12 @@
-(defproject lupapiste/geo-conversion "0.1.1-SNAPSHOT"
-  :description "epsg3067 to wgs84 conversion library"
-  :url "http://www.solita.fi"
+(defproject lupapiste/geo-conversion "0.2.0"
+  :description "Coordinate conversion library"
+  :url "https://github.com/lupapiste/geo-conversion"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"
             :distribution :repo}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [org.geotools/gt-main "14.2"]
-                 [org.geotools/gt-referencing "14.2"]
-                 [org.geotools/gt-epsg-wkt "14.2"]]
+                 [org.geotools/gt-main "17.1"]
+                 [org.geotools/gt-referencing "17.1"]
+                 [org.geotools/gt-epsg-wkt "17.1"]]
   :aot :all
   :repositories [["osgeo" {:url "http://download.osgeo.org/webdav/geotools"}]])
--- src/geo_conversion/core.clj
+++ src/geo_conversion/core.clj
@@ -6,15 +6,22 @@
 (defn- round-to [n acc]
   (.setScale n acc BigDecimal/ROUND_HALF_UP))
 
-(defn epsg3067->wgs84 [coord-array]
-  (let [source-CRS (CRS/decode "epsg:3067")
-        to-CRS DefaultGeographicCRS/WGS84
+(def wgs-84 DefaultGeographicCRS/WGS84)
+
+(def etrs-tm35fin "epsg:3067")
+
+(defn convert-crs [coord-array src-crs to-crs]
+  (let [source-CRS (CRS/decode src-crs)
+        to-CRS (if (= wgs-84 to-crs) wgs-84 (CRS/decode to-crs))
         math-transform (CRS/findMathTransform source-CRS to-CRS true)
-        direct-pos (->> coord-array
-                        (map (comp #(.doubleValue %) bigdec))
-                        (into-array Double/TYPE)
-                        GeneralDirectPosition.)
+        double-array (->> coord-array
+                          (map (comp #(.doubleValue %) bigdec))
+                          (into-array Double/TYPE))
+        direct-pos (GeneralDirectPosition. ^doubles double-array)
         result-point (. math-transform transform direct-pos nil)]
     (->> result-point
          .getCoordinate
          (map (comp #(.doubleValue %) #(round-to % 5) bigdec)))))
+
+(defn epsg3067->wgs84 [coord-array]
+  (convert-crs coord-array etrs-tm35fin wgs-84))
