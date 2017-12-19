501db12e3f7ac1d8fa17a26b118d852d587ac1f2
Markus Penttila <markus.penttila@evolta.fi>
2017-12-19 15:06:00 +0200

    Make project compatible with newer Leiningen versions

--- project.clj
+++ project.clj
@@ -1,4 +1,8 @@
-(defproject lupapiste/geo-conversion "0.2.0"
+(require 'cemerick.pomegranate.aether)
+(cemerick.pomegranate.aether/register-wagon-factory!
+  "http" #(org.apache.maven.wagon.providers.http.HttpWagon.))
+
+(defproject lupapiste/geo-conversion "0.2.1"
   :description "Coordinate conversion library"
   :url "https://github.com/lupapiste/geo-conversion"
   :license {:name "Eclipse Public License"
@@ -9,4 +13,5 @@
                  [org.geotools/gt-referencing "17.1"]
                  [org.geotools/gt-epsg-wkt "17.1"]]
   :aot :all
-  :repositories [["osgeo" {:url "http://download.osgeo.org/webdav/geotools"}]])
+  :repositories [["boundless" {:url "https://repo.boundlessgeo.com/main/"}]
+                 ["osgeo" {:url "http://download.osgeo.org/webdav/geotools"}]])
