d5d2cb7fcb5e8f9d0cad43f6e5bc7fbf75582325
Markus Penttila <markus.penttila@evolta.fi>
2019-06-10 13:20:46 +0300

    Use geotools 21.1

--- project.clj
+++ project.clj
@@ -1,14 +1,12 @@
-(require 'cemerick.pomegranate.aether)
-
-(defproject lupapiste/geo-conversion "0.2.2"
+(defproject lupapiste/geo-conversion "0.3.0"
   :description "Coordinate conversion library"
   :url "https://github.com/lupapiste/geo-conversion"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"
             :distribution :repo}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [org.geotools/gt-main "19.1"]
-                 [org.geotools/gt-referencing "19.1"]
-                 [org.geotools/gt-epsg-wkt "19.1"]]
+                 [org.geotools/gt-main "21.1"]
+                 [org.geotools/gt-referencing "21.1"]
+                 [org.geotools/gt-epsg-wkt "21.1"]]
   :aot :all
   :repositories [["osgeo" {:url "https://download.osgeo.org/webdav/geotools"}]])
