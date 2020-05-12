f32d3349808dce045bbb7f16aea5be56c9b42ba5
Markus Penttila <markus.penttila@cloudpermit.com>
2020-05-12 14:18:44 +0300

    Update osgeo repo and deps

--- project.clj
+++ project.clj
@@ -1,12 +1,12 @@
-(defproject lupapiste/geo-conversion "0.3.0"
+(defproject lupapiste/geo-conversion "0.4.0"
   :description "Coordinate conversion library"
   :url "https://github.com/lupapiste/geo-conversion"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"
             :distribution :repo}
-  :dependencies [[org.clojure/clojure "1.8.0"]
-                 [org.geotools/gt-main "21.1"]
-                 [org.geotools/gt-referencing "21.1"]
-                 [org.geotools/gt-epsg-wkt "21.1"]]
+  :dependencies [[org.clojure/clojure "1.10.1"]
+                 [org.geotools/gt-main "23.0"]
+                 [org.geotools/gt-referencing "23.0"]
+                 [org.geotools/gt-epsg-wkt "23.0"]]
   :aot :all
-  :repositories [["osgeo" {:url "https://download.osgeo.org/webdav/geotools"}]])
+  :repositories [["osgeo" {:url "https://repo.osgeo.org/repository/release/"}]])
