c9fe3b3dff00339297de671ffeb3f3577d17f439
Markus Penttila <markus.penttila@cloudpermit.com>
2022-11-22 17:59:23 +0200

    Update dependencies, bump version to 0.5.0

--- project.clj
+++ project.clj
@@ -1,12 +1,20 @@
-(defproject lupapiste/geo-conversion "0.4.0"
+(defproject lupapiste/geo-conversion "0.5.0"
   :description "Coordinate conversion library"
   :url "https://github.com/lupapiste/geo-conversion"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"
             :distribution :repo}
-  :dependencies [[org.clojure/clojure "1.10.1"]
-                 [org.geotools/gt-main "23.0"]
-                 [org.geotools/gt-referencing "23.0"]
-                 [org.geotools/gt-epsg-wkt "23.0"]]
+  :dependencies [[org.clojure/clojure "1.11.1"]
+                 [org.eclipse.emf/org.eclipse.emf.common "2.26.0"]
+                 [org.eclipse.emf/org.eclipse.emf.ecore "2.28.0" :exclusions [org.eclipse.emf/org.eclipse.emf.common]]
+                 [org.geotools/gt-main "28.0" :exclusions [org.eclipse.emf/org.eclipse.emf.common
+                                                           org.eclipse.emf/org.eclipse.emf.ecore
+                                                           javax.media/jai_core]]
+                 [org.geotools/gt-referencing "28.0" :exclusions [org.eclipse.emf/org.eclipse.emf.common
+                                                                  org.eclipse.emf/org.eclipse.emf.ecore
+                                                                  javax.media/jai_core]]
+                 [org.geotools/gt-epsg-wkt "28.0" :exclusions [org.eclipse.emf/org.eclipse.emf.common
+                                                               org.eclipse.emf/org.eclipse.emf.ecore
+                                                               javax.media/jai_core]]]
   :aot :all
   :repositories [["osgeo" {:url "https://repo.osgeo.org/repository/release/"}]])
--- src/geo_conversion/core.clj
+++ src/geo_conversion/core.clj
@@ -1,19 +1,23 @@
 (ns geo-conversion.core
   (:import [org.geotools.referencing.crs DefaultGeographicCRS]
            [org.geotools.referencing CRS]
-           [org.geotools.geometry GeneralDirectPosition]))
+           [org.geotools.geometry GeneralDirectPosition]
+           [org.opengis.referencing.crs CoordinateReferenceSystem]
+           [java.math RoundingMode]))
 
-(defn- round-to [n acc]
-  (.setScale n acc BigDecimal/ROUND_HALF_UP))
+(defn- round-to [^BigDecimal n acc]
+  (.setScale n (int acc) RoundingMode/HALF_UP))
 
 (def wgs-84 DefaultGeographicCRS/WGS84)
 
 (def etrs-tm35fin "epsg:3067")
 
-(defn convert-crs [coord-array src-crs to-crs]
+(defn convert-crs [coord-array ^String src-crs ^String to-crs]
   (let [source-CRS (CRS/decode src-crs)
         to-CRS (if (= wgs-84 to-crs) wgs-84 (CRS/decode to-crs))
-        math-transform (CRS/findMathTransform source-CRS to-CRS true)
+        math-transform (CRS/findMathTransform ^CoordinateReferenceSystem source-CRS
+                                              ^CoordinateReferenceSystem to-CRS
+                                              true)
         double-array (->> coord-array
                           (map (comp #(.doubleValue %) bigdec))
                           (into-array Double/TYPE))
